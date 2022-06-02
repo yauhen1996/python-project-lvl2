81379a008485fefb5f4989b96182e16598d5fb3e
yauhen1996 <getthe798@gmail.com>
2022-06-02 19:07:01 +0300

    workflows

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ build: #
 
 package-install: #
 	        python3 -m pip install --user dist/*.whl --force-reinstall
+
+lint: #
+	poetry run flake8 gendiff
--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,5 +1,5 @@
 import json
-import itertools
+import re
 
 
 def generate_diff(f_1, f_2):
@@ -7,22 +7,22 @@ def generate_diff(f_1, f_2):
         file1 = json.load(f)
     with open("/home/yauhen1996/python-project-lvl2/tests/fixtures/file2.json") as f:
         file2 = json.load(f)
-    f1 = file1.items()
-    f2 = file2.items()
-    l = []
-    for k1, v1 in f1:
-        for k2, v2 in f2:
-            if (k1, v1) in f2:
-                l.append(f'  {k1}: {v1}')
-            if k1 in (k2, v2) and v1 not in (k2, v2):
-                l.extend([f'+ {k2}: {v2}'])
-                l.extend([f'- {k1}: {v1}'])
-            if (k1, v1) not in f2:
-                l.extend([f'- {k1}: {v1}'])
-            if (k2, v2) not in f1:
-                l.extend([f'+ {k2}: {v2}'])
-    del_repeat = list(set(l))
-    del_repeat.sort(key=lambda x: x[2])                         
-    build = itertools.chain("{", del_repeat, "}")
-    result = '\n'.join(build)
-    return result
+    dict1 = {}
+    dict2 = {}
+    file_1 = file1.items()
+    file_2 = file2.items()
+    set_f = file_1 | file_2
+    for item in list(set_f):
+        if item in file_1 and item in file_2:
+            dict1[f'  {item[0]}'] = item[1]
+        if item in file_1 and item not in file_2:
+            dict1[f'- {item[0]}'] = item[1]
+        if item in file_2 and item not in file_1:
+            dict2[f'+ {item[0]}'] = item[1]
+    sort1 = sorted(dict1.items(), key=lambda x: x[0][2])
+    sort2 = sorted(dict2.items(), key=lambda x: x[0][2])
+    general = dict(sort1 + sort2)
+    gen_sort = sorted(general.items(), key=lambda x: x[0][2])
+    result = json.dumps(dict(gen_sort), indent=2)
+    clear_result = re.sub('\"|,', '', result)
+    return clear_result

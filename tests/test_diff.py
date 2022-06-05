562fb37d38ffd0d65da4cd642cf32e4eb76511bc
yauhen1996 <getthe798@gmail.com>
2022-06-05 17:11:20 +0300

    all

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -9,9 +9,9 @@ import pytest
 def test_diff():
     import json
     import re
-    data = json.load(open("/home/yauhen1996/python-project-lvl2/tests/fixtures/result.json"))
+    data = json.load(open("tests/fixtures/result.json"))
     result = json.dumps(dict(data), indent=2)
     clear = re.sub('\"|,', '', result)
-    file_1 = json.load(open("/home/yauhen1996/python-project-lvl2/tests/fixtures/file.json"))
-    file_2 = json.load(open("/home/yauhen1996/python-project-lvl2/tests/fixtures/file2.json"))
+    file_1 = json.load(open("tests/fixtures/file.json"))
+    file_2 = json.load(open("tests/fixtures/file2.json"))
     assert clear == generate_diff(file_1, file_2)

eb2bd8ae4751f08a91a7e28d088b460bb5bcd194
yauhen1996 <getthe798@gmail.com>
2022-06-08 11:11:04 +0300

    all

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -4,10 +4,8 @@ import yaml
 
 def parse(data1, data2):
     if data1.endswith('.json') and data2.endswith('.json'):
-        file1_j = json.load(open("tests/fixtures/file.json"))
         file2_j = json.load(open("tests/fixtures/file2.json"))
-        return file1_j, file2_j
+        return json.load(open("tests/fixtures/file.json")), file2_j
     elif data1.endswith(('yml', 'yaml')) and data2.endswith(('yml', 'yaml')):
-        file1_y = yaml.safe_load(open("tests/fixtures/file1.yaml"))
         file2_y = yaml.safe_load(open("tests/fixtures/file2.yml"))
-        return file1_y, file2_y
+        return yaml.safe_load(open("tests/fixtures/file1.yaml")), file2_y

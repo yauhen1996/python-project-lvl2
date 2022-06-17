510385019473dc001b6db70143e0d79ad3a961f1
yauhen1996 <getthe798@gmail.com>
2022-06-17 13:48:27 +0300

    .

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -7,13 +7,13 @@ def stylish(arg):
         res = {}
         for key, val in on.items():
             if val["type"] == "nested":
-                res[f'  {key}'] = call(val["children"])
+                res[f'{key}'] = call(val["children"])
             elif val["type"] == "added":
                 res[f'+ {key}'] = val["value"]
             elif val["type"] == "deleted":
                 res[f'- {key}'] = val["value"]
             elif val["type"] == "unchanged":
-                res[f'  {key}'] = val["value"]
+                res[f'{key}'] = val["value"]
             elif val["type"] == "changed":
                 res[f'- {key}'] = val["value_1"]
                 res[f'+ {key}'] = val["value_2"]
@@ -21,4 +21,12 @@ def stylish(arg):
     data = call(arg)
     string = json.dumps(data, indent=4)
     clear = re.sub('\"|,', '', string)
-    return clear
+    result = list(clear)
+    for i, v in enumerate(result):
+        if v == '+':
+            del result[i - 1]
+            del result[i - 2]
+        elif v == '-':
+            del result[i - 1]
+            del result[i - 2]
+    return ''.join(result)
